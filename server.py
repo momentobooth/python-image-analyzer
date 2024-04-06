01ed5913e9901899769acd7c0819995764b8e20c
Casper Cromjongh <casper.cromjongh@hotmail.com>
2024-04-06 15:28:35 +0200

    Add args for directories, add filesystem watchers.

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
+import argparse
+
 from flask import Flask, request, send_from_directory, jsonify
+from watchdog.events import FileSystemEventHandler, FileSystemEvent
+from watchdog.observers import Observer
 from PIL import Image
 from momento_booth import *
 from momento_booth_image_search import get_matching_images
@@ -38,8 +42,41 @@ def get_matching_imgs():
     return jsonify(matching_images)
 
 
+class CollageWatcherHandler(FileSystemEventHandler):
+    def on_created(self, event: FileSystemEvent) -> None:
+        print(f"Collage got created, {event.src_path}")
+        pass
+
+
+class SourceWatcherHandler(FileSystemEventHandler):
+    def on_created(self, event: FileSystemEvent) -> None:
+        print(f"Source image got created, {event.src_path}")
+        pass
+
+
+def watcher(collage_path: Path, source_path: Path):
+    observer = Observer()
+    observer.schedule(CollageWatcherHandler(), path=str(collage_path), recursive=False)
+    observer.schedule(SourceWatcherHandler(), path=str(source_path), recursive=False)
+    observer.start()
+
+
+def directory_type(raw_path: str) -> Path:
+    p = Path(raw_path)
+    if not p.is_dir():
+        raise argparse.ArgumentTypeError('"{}" is not an existing directory'.format(raw_path))
+    return p
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="MomentoBooth image processing companion server")
+    parser.add_argument("--collage-dir", type=directory_type, help="Output directory")
+    parser.add_argument("--source-dir", type=directory_type, help="Input file(s) or directory")
+    args = parser.parse_args()
+    print(f"Running with \n - Collage dir: {args.collage_dir}\n - Source dir: {args.source_dir}")
+
     data_file_path = Path("data.json")
     with open(data_file_path, "r") as f:
         data: dict = json.loads(f.read())
-    app.run(host="::", port=5000, debug=True)
+    watcher(args.collage_dir, args.source_dir)
+    app.run(host="::", port=5000, debug=True, use_reloader=False)
