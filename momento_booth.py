7262363cc875949c0e916d549b32d9e4a86afbd8
Casper Cromjongh <casper.cromjongh@hotmail.com>
2024-04-11 17:26:10 +0200

    Save more information.
    Including faces :)

--- momento_booth.py
+++ momento_booth.py
@@ -20,13 +20,14 @@ def detect_people(model: YOLO, img: Path) -> yolo_results.Results:
 
 
 class FacesResult:
-    def __init__(self, encodings: np.ndarray, detection, encoding):
+    def __init__(self, encodings: np.ndarray, locations: list[tuple[int, ...]], detection, encoding):
         self.encodings = encodings
+        self.locations = locations
         self.speed = {'detection': detection*1000, 'encoding': encoding*1000}
 
 
 def detect_faces(image: Path | Image.Image):
-    pillow_source_img = Image.open(image) if issubclass(type(image), Path) else image
+    pillow_source_img = Image.open(image) if issubclass(type(image), Path) else image.copy()
 
     original_size = pillow_source_img.size
     pillow_source_img.thumbnail((600, 600))
@@ -39,22 +40,37 @@ def detect_faces(image: Path | Image.Image):
     return face_locations
 
 
-def get_faces(sources: list[Path | Image.Image]) -> FacesResult:
+def get_faces(sources: list[Path | Image.Image], collage_path: Path | None = None) -> FacesResult:
+    """
+    Get faces and encodings from the best of a list of images.
+
+    :param sources: Sources to look through
+    :return: Results
+    """
     face_lock.acquire(blocking=True)
     start_detecting_face = time.time()
     imgs_faces = [detect_faces(source) for source in sources]
     num_faces = [len(faces) for faces in imgs_faces]
-    best_photo = num_faces.index(max(num_faces))
+    best_photo_index = num_faces.index(max(num_faces))
 
+    best_photo = sources[best_photo_index]
     # Load the jpg file into a numpy array
-    pillow_source_img = Image.open(sources[best_photo]) if issubclass(type(sources[best_photo]), Path) else sources[best_photo]
+    pillow_source_img = Image.open(best_photo) if issubclass(type(best_photo), Path) else best_photo
     np_image = np.array(pillow_source_img)
 
+    if collage_path is not None:
+        for (index, face) in enumerate(imgs_faces[best_photo_index]):
+            top, right, bottom, left = face
+            face_image = np_image[top:bottom, left:right]
+            pil_image = Image.fromarray(face_image)
+            filename = f"{collage_path.stem}-face-{index+1}.jpg"
+            pil_image.save(collage_path.parent.joinpath("faces", filename))
+
     start_encoding_face = time.time()
-    face_encodings = face_recognition.face_encodings(np_image, imgs_faces[best_photo])
+    face_encodings = face_recognition.face_encodings(np_image, imgs_faces[best_photo_index])
     face_end = time.time()
     face_lock.release()
-    return FacesResult(face_encodings, start_encoding_face - start_detecting_face, face_end - start_encoding_face)
+    return FacesResult(face_encodings, imgs_faces[best_photo_index], start_encoding_face - start_detecting_face, face_end - start_encoding_face)
 
 
 def find_image(name: str, collage_path: Path, source_dir: Path) -> Path | None:
@@ -85,33 +101,36 @@ def process_collage(collage: Path, source_dir: Path, model: YOLO, already_proces
     exif_dict = piexif.load(pillow_collage_img.info["exif"])
     json_str = exif_dict["Exif"][piexif.ExifIFD.MakerNote]
     json_obj = json.loads(json_str)
-    print(f"\tJSON object: {json_obj}")
-    raw_sources = json_obj["sourcePhotos"]
-    found_sources = (find_image(source['filename'], collage, source_dir) for source in raw_sources)
+    raw_sources = [source['filename'] for source in json_obj["sourcePhotos"]]
+    found_sources = (find_image(source, collage, source_dir) for source in raw_sources)
     sources = [s for s in found_sources if s is not None]
+    print(f"\toriginal sources: {raw_sources}, resolved = {[f.name for f in sources]}")
     if already_processed:
         return None
-
-    date_p = date_from_exif(exif_dict)
-    date_s = date_p.strftime("%Y%m%d_%H")
 
     if len(sources) == 0:
         print("\tCould not find source files, skipping")
         return None
-    else:
-        print(f"\tImage has {len(sources)} source(s)")
     last_source = sources[-1]
 
     result = detect_people(model, last_source)
     print(f"\tDetected {len(result.boxes)} people in {sum(result.speed.values()):.1f} ms")
 
-    face_results = get_faces(sources)
+    face_results = get_faces(sources, collage)
     print(f"\tFound {len(face_results.encodings)} face(s). Locating {face_results.speed['detection']:.1f} ms, encoding {face_results.speed['encoding']:.1f} ms.")
     end = time.time()
 
     return {
-        "people": len(result.boxes),
-        "faces": [encodings.tolist() for encodings in face_results.encodings]
+        "people_count": len(result.boxes),
+        "sources": {
+            "original": raw_sources,
+            "resolved": [f.name for f in sources]
+        },
+        "faces": {
+            "count": len(face_results.encodings),
+            "locations": face_results.locations,
+            "encodings": [encodings.tolist() for encodings in face_results.encodings]
+        }
     }
 
 
